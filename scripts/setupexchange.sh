920a988d1469bc37e0d1c5b51143f5bd62507e3e
meuchels <sam@meuchel.biz>
2023-10-09 21:00:54 -0500

    modified setup to check for overwrite of files

--- scripts/setupexchange.sh
+++ scripts/setupexchange.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if the secrets file exists
+if [ ! -f ../secrets ]; then
+    echo "Error: 'secrets' file not found. Please create it with the necessary values."
+    exit 1
+fi
+
 # Request user input for SHORT_TENANT_NAME
 read -p "Enter a short tenant name: " TENANT_SHORT_NAME
 
@@ -14,6 +20,19 @@ read -p "Enter your AZURE_CLIENT_SECRET: " AZURE_CLIENT_SECRET
 
 # Source the secrets file
 source ../secrets
+
+# Define file paths
+environment_file="./environments/$SHORT_TENANT_NAME-exchange"
+backup_file="./back-available/$SHORT_TENANT_NAME-exchange"
+
+# Check if environment file already exists
+if [ -f "$environment_file" ]; then
+    read -p "The environment file '$environment_file' already exists. Do you want to overwrite it? (y/n) " overwrite_env
+    if [ "$overwrite_env" != "y" ]; then
+        echo "Aborted. Exiting..."
+        exit 0
+    fi
+fi
 
 # Define the content of the environment file
 content_environment=$(cat <<EOF
@@ -47,11 +66,23 @@ EOF
 # Create the environments directory if it doesn't exist
 mkdir -p ./environments
 
-# Write the content to the file
-echo "$content_environment" > "./environments/$SHORT_TENANT_NAME-exchange"
+# Check if environment file already exists
+if [ -f "$environment_file" ]; then
+    read -p "The environment file '$environment_file' already exists. Do you want to overwrite it? (y/n) " overwrite_env
+    if [ "$overwrite_env" != "y" ]; then
+        echo "Aborted. Exiting..."
+        exit 0
+    fi
+fi
 
-# Provide some feedback
-echo "File './environments/$SHORT_TENANT_NAME-exchange' created."
+# Check if backup script file already exists
+if [ -f "$backup_file" ]; then
+    read -p "The backup script file '$backup_file' already exists. Do you want to overwrite it? (y/n) " overwrite_backup
+    if [ "$overwrite_backup" != "y" ]; then
+        echo "Aborted. Exiting..."
+        exit 0
+    fi
+fi
 
 # Define the content of the backup script file
 content_backup=$(cat <<EOF
@@ -81,10 +112,12 @@ EOF
 # Create the back-available directory if it doesn't exist
 mkdir -p ./back-available
 
-# Write the content to the file
-echo "$content_backup" > "./back-available/$SHORT_TENANT_NAME-exchange"
-
-# Provide some feedback
-echo "File './back-available/$SHORT_TENANT_NAME-exchange' created."
+# Write the content to the file if it doesn't exist or if user confirms overwrite
+if [ ! -f "$backup_file" ] || [ "$overwrite_backup" == "y" ]; then
+    echo "$content_backup" > "$backup_file"
+    echo "File '$backup_file' created."
+else
+    echo "File '$backup_file' already exists. Skipped creation."
+fi
 
 echo "Don't forget to activate your backup!"
